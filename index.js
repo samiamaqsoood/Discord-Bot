166ac8900ef65803164518663513314e9a6c2fd3
Samia Maqsood <samiamaqsoood@gmail.com>
2025-07-27 18:34:46 +0500

    C:/Program Files/Git/commands using bot

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const client = new Client({ intents: [
 
 client.on("messageCreate", (message)=>{
     console.log(message.content)
+    if(message.author.bot) return;
+    if(message.content.startsWith("create")){
+        const url = message.content.split("create")[1];
+        return message.reply({
+            content : "Genrating short ID for" + url,
+        })
+    }
+    message.reply({
+        content: "Hi from bot!"
+    })
+})
+
+client.on("interactionCreate", (interaction) =>{
+    console.log(interaction);
+    interaction.reply("Pong!!")
 })
 
 client.login(token);
