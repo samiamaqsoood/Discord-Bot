15e370143f2b7fd77fd5e194ce4fbaaa310db151
Samia Maqsood <samiamaqsoood@gmail.com>
2025-07-29 17:33:32 +0500

    create short urls using create keyword

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-const {Client, GatewayIntentBits} = require("discord.js");
-const {token} = require("./token")
-//intent means type o permission
-const client = new Client({ intents: [
-     GatewayIntentBits.Guilds,
-     GatewayIntentBits.GuildMessages,
-     GatewayIntentBits.MessageContent
-    ] });
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token } = require("./token");
+const { connectToMongoDB } = require("./connection");
+const fetch = require("node-fetch");
 
-client.on("messageCreate", (message)=>{
-    console.log(message.content)
-    if(message.author.bot) return;
-    if(message.content.startsWith("create")){
-        const url = message.content.split("create")[1];
-        return message.reply({
-            content : "Genrating short ID for" + url,
-        })
-    }
-    message.reply({
-        content: "Hi from bot!"
-    })
-})
+//intent means type of permission
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
-client.on("interactionCreate", (interaction) =>{
-    console.log(interaction);
-    interaction.reply("Pong!!")
-})
+client.on("messageCreate", async (message) => {
+  console.log(message.content);
+  if (message.author.bot) return;
+  if (message.content.startsWith("create")) {
+    const url = message.content.split("create")[1];
+
+    const res = await fetch("http://localhost:8001/api/bot", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: url.trim(),
+      }),
+    });
+    const data = await res.json();
+    return message.reply({
+      content: "Genrated short ID = " + "http://localhost:8001/"+ data.id,
+    });
+  }
+  message.reply({
+    content: "Hi from bot!",
+  });
+});
+
+client.on("interactionCreate", (interaction) => {
+  console.log(interaction);
+  interaction.reply("Pong!!");
+});
 
 client.login(token);
